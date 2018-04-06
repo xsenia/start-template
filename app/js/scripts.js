3f7b3798a5b3dbd63c306387ccbc0440c84ca1ac
Ksenia <xsanka30@gmail.com>
2018-04-06 23:18:37 +0500

    header

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,3 +1,7 @@
-window.onload=function(){
-	console.log('Hello Gulp!');
+var buttonMobile = document.getElementById('mobile-menu-button');
+var menuDropdownMobile = document.getElementById('header__mobile-dropdown');
+
+buttonMobile.onclick = function() {
+  buttonMobile.classList.toggle('mobile-menu-button_active');
+  menuDropdownMobile.classList.toggle('header__mobile-dropdown_open');
 }
