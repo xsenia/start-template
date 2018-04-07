f65d7a8e21b587f54ee60c3710624dffcaab2be4
Ksenia <xsanka30@gmail.com>
2018-04-08 01:05:44 +0500

    footer, main-block, accordeon

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -5,3 +5,28 @@ buttonMobile.onclick = function() {
   buttonMobile.classList.toggle('mobile-menu-button_active');
   menuDropdownMobile.classList.toggle('header__mobile-dropdown_open');
 }
+
+var collapseWrap = document.getElementsByClassName('collapse-wrap');
+      
+for (var i = 0; i < collapseWrap.length; i++) {
+
+collapseWrap[i].addEventListener("click", collapse, false);
+
+collapseWrap[i].onmousedown = function(event){	
+  collapseWrap[i].onmouseup = function(){
+    collapseWrap[i].click = null;
+  }
+}
+
+}
+
+function collapse() {
+if (this.classList.contains('open')) {
+  for (var i = 0; i < collapseWrap.length; i++) {
+    this.classList.remove('open');
+  }          
+} else {
+  this.classList.add('open');
+}
+}
+
