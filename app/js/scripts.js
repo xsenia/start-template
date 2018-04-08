93b31f8de3227f5276f1bda7f7507b01c5405296
Ksenia <xsanka30@gmail.com>
2018-04-09 01:01:00 +0500

    first page

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,36 +1,27 @@
 //header-mobile-menu
 var buttonMobile = document.getElementById('mobile-menu-button');
-var menuDropdownMobile = document.getElementById('header__mobile-dropdown');
+var menuDropdownMobile = document.getElementById('header-mobile-dropdown');
 
 buttonMobile.onclick = function() {
   buttonMobile.classList.toggle('mobile-menu-button_active');
-  menuDropdownMobile.classList.toggle('header__mobile-dropdown_open');
+  menuDropdownMobile.classList.toggle('header-mobile-dropdown_open');
 }
 
 
 
 //collapse-block
-var collapseWrap = document.getElementsByClassName('collapse__item');
-      
-for (var i = 0; i < collapseWrap.length; i++) {
-
-  collapseWrap[i].addEventListener("click", collapse, false);
-
-  collapseWrap[i].onmousedown = function(event){  
-    collapseWrap[i].onmouseup = function(){
-      collapseWrap[i].click = null;
-    }
-  }
-
+var collapseHeader = document.getElementsByClassName('collapse__header');      
+for (var i = 0; i < collapseHeader.length; i++) {
+  collapseHeader[i].addEventListener("click", collapse, false);
 }
-
 function collapse() {
-if (this.classList.contains('open')) {
-  for (var i = 0; i < collapseWrap.length; i++) {
-    this.classList.remove('open');
-  }          
-} else {
-  this.classList.add('open');
-}
+  parent = this.parentNode;
+  if (parent.classList.contains('open')) {
+    for (var i = 0; i < collapseHeader.length; i++) {
+      parent.classList.remove('open');
+    }          
+  } else {
+    parent.classList.add('open');
+  }
 }
 
